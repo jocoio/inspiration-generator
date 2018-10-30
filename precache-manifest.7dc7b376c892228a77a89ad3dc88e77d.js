a01bf5442ef8ff35e6857bec16d107a04fe6c28b
Jon Corbett <corbett.jo@husky.neu.edu>
2018-10-29 21:02:51 -0400

    Updates

---
+++ precache-manifest.7dc7b376c892228a77a89ad3dc88e77d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77329d9bd7f5e66dc0b3",
+    "url": "/static/css/main.1236ad3e.chunk.css"
+  },
+  {
+    "revision": "77329d9bd7f5e66dc0b3",
+    "url": "/static/js/main.77329d9b.chunk.js"
+  },
+  {
+    "revision": "5940df1376023eb19173",
+    "url": "/static/js/1.5940df13.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cbd6ce8e8e006338e5c0709a649b17d",
+    "url": "/index.html"
+  }
+];
